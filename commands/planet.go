839b7ee45664f400d6e0f00417a65ac6f0b0d61b
Dan McCracken <mccrackend@gmail.com>
2014-07-21 11:05:32 -0700

    initial commit from step 2

---
+++ commands/planet.go
@@ -0,0 +1,56 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+/*
+var RootCmd = &cobra.Command{
+	Use:   "dagobah",
+	Short: `Dagobah is an awesome planet style RSS aggregator`,
+	Long:  `RSS agg + YAML and web blah blah`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Dagobah runs")
+	},
+}
+*/
+
+func Execute() {
+	err := RootCmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+var CfgFile string
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	RootCmd.PersistentFlags().StringVar(&CfgFile, "config", "", "config file (default is $HOME/dagobah/config.yaml)")
+}
+
+func initConfig() {
+	if CfgFile != "" {
+		viper.SetConfigFile(CfgFile)
+	}
+	viper.SetConfigName("config")
+	viper.AddConfigPath("/etc/dagobah/")
+	viper.AddConfigPath("$HOME/.dagobah/")
+	viper.ReadInConfig()
+}
+
+var RootCmd = &cobra.Command{
+	Use: "",
+	Short: `...`,
+	Long: `...`,
+	Run: rootRun,
+}
+
+func rootRun(cmd *cobra.Command, args []string) {
+	fmt.Println(viper.Get("feeds"))
+	fmt.Println(viper.GetString("appname"))
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/mccrackend/myProject/commands"
+
+func main() {
+	commands.Execute()
+}
